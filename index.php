552d6aec248357612bd7d403ffd5c6ebdcdcece7
MFaridgunawan <m.fgunawan15@gmail.com>
2024-08-26 13:21:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap with Composer</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Halo dunia saya Farid!!</h1>
+        <p class="lead text-center">Membuat halaman web sebagai contoh memakai package Bootstrap.</p>
+        <div class="text-center">
+            <a href="#" class="btn btn-primary">Tekan Aku</a>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
